67df244594f18b3577572ba8daa99c78175d4122
Riku Mattila <riku.p.mattila@gmail.com>
2021-11-29 19:06:59 +0200

    tehtiin toien testi

--- tdd-ostoskori/src/ostoskori.py
+++ tdd-ostoskori/src/ostoskori.py
@@ -18,6 +18,7 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
+        pass
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
