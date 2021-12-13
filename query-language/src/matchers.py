379454f4bd566e1656e2b0b8379fb58f19da742c
Riku Mattila <riku.p.mattila@gmail.com>
2021-12-13 19:17:45 +0200

    viikon 6 tehtävät tehty

--- query-language/src/matchers.py
+++ query-language/src/matchers.py
@@ -10,58 +10,72 @@ class And:
         return True
 
 class PlaysIn:
-    def __init__(self, team):
+    def __init__(self, query, team):
         self._team = team
+        self.query=query
 
     def matches(self, player):
         return player.team == self._team
 
 class HasAtLeast:
-    def __init__(self, value, attr):
+    def __init__(self, query, value, attr):
         self._value = value
         self._attr = attr
+        self.query=query
 
     def matches(self, player):
         player_value = getattr(player, self._attr)
 
-        return player_value >= self._value
+        return (player_value >= self._value and self.query.matches(player))
 
 class All:
     def __init__(self, *matchers):
             self._matchers = matchers
-    
+
     def matches(self, player):
         return True
 
 class HasFewerThan:
-    def __init__(self, value, attr):
+    def __init__(self, query, value, attr):
         self._value=value
         self._attr=attr
+        self.query=query
     def matches(self, player):
         player_value = getattr(player, self._attr)
 
-        return player_value < self._value
+        return (player_value < self._value and self.query.matches(player))
 
 class Not:
-    def __init__(self, *matchers):
+    def __init__(self, query, *matchers):
         self._matchers = matchers
+        self.query=query
     
     def matches(self, player):
         for matcher in self._matchers:
-            if matcher.matches(player):
+            if not self.query.matches(player) or matcher.matches(player):
                 return False
-        
         return True
 
 class Or:
-    def __init__(self, *matchers):
-        self._matchers = matchers
-
+    def __init__(self, queries):
+        self._queries=queries
     def matches(self, player):
-        for matcher in self._matchers:
-            if matcher.matches(player):
+        for i in self._queries:
+            if i.matches(player):
                 return True
         return False
 
-
-
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self.query=query
+        pass
+    def playsIn(self, team):
+        return QueryBuilder(PlaysIn(self.query, team))
+    def hasAtLeast(self, num, attr):
+        return QueryBuilder(HasAtLeast(self.query, num, attr))
+    def hasFewerThan(self, num, attr):
+        return QueryBuilder(HasFewerThan(self.query, num, attr))
+    def oneOf(self, *args):
+        return QueryBuilder(Or(args))
+    def build(self):
+        return self.query
